b653c0b9d756ff8eab7aea88441ed2ac0f12c46f
panagiotismrowca <89362761+panagiotismrowca@users.noreply.github.com>
2023-11-05 18:35:43 +0200

    add prisma

--- src/app/page.js
+++ src/app/page.js
@@ -2,28 +2,14 @@ import Image from "next/image";
 import { Station } from "@/components/Station";
 import { Map } from "@/components/Map";
 import { Report } from "@/components/Report";
+import User from "@/components/User";
+import { PrismaClient } from '@prisma/client';
 
 export default function Home() {
   return (
     <div class="justify-between">
     <Map />
     <Report />
-    <Station />
-    <Station />
-    <Station />
-    <Station />
-    <Station />
-    <Station />
-    <Station />
-    <Station />
-    <Station />
-    <Station />
-    <Station />
-    <Station />
-    <Station />
-    <Station />
-    <Station />
-    <Station />
     <Station />
     </div>
   )
