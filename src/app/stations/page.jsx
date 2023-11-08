9a23cf14a3dea1b39b3dd6e00dd06668cf8d0ab8
panagiotismrowca <89362761+panagiotismrowca@users.noreply.github.com>
2023-11-08 23:46:09 +0200

    ui

--- src/app/stations/page.jsx
+++ src/app/stations/page.jsx
@@ -1,43 +1,12 @@
 import Map from "@/components/Map"
 import StationList from "@/components/StationList"
-import ServerComponentExample from "@/components/ServerComponentExample"
-import Link from 'next/link';
 import React from 'react';
-import Image from 'next/image';
 
 const page = () => {
   return (
     <main className="flex flex-grow flex-col items-center justify-center space-y-10">
       <Map />
-
       <StationList />
-      
-
-      <Link href="stations/1">
-        <div className="w-full p-2 border border-gray-500 rounded-md flex items-center">
-          <div className="fw-3/10 h-full flex items-center justify-center">
-            <Image
-              src="/icons/exclamationcircle.svg"
-              width={18}
-              height={18}
-              alt="Map icon"
-            />
-          </div>
-    <div class="w-7/10 h-full flex items-center justify-center">
-        ΣΤΑΘΜΟΣ
-    </div>
-            <h2 className="text-lg font-semibold ml-1"> Maps </h2>
-        </div>
-        <div className="w-1/10 h-full flex items-center justify-center">
-            <Image
-              src="/icons/exclamationcircle.svg"
-              width={18}
-              height={18}
-              alt="Map icon"
-            />
-          </div>
-      </ Link>
-      
     </main>
   )
 }
--- src/components/StationList.jsx
+++ src/components/StationList.jsx
@@ -5,29 +5,37 @@ import Image from "next/image"
 const StationList = async () => {
   const supabase = createServerComponentClient({ cookies })
 
-  const { data, error } = await supabase.from("stations").select("*")
+  // let { data: stations, error } = await supabase.from('stations').select(`
+  //     station_name,
+  //     stations_routes( station_id , route_id )
+  // `).eq('stations_routes.route_idgfd', 2)
+  let { data: stations, error } = await supabase.rpc("get_stations_with_routes", {route_index: 1});
+
+  let lines = [];
 
   return(
     <>
-        {data.map((station) => (
-          <a href={'stations/' + station.id} className=" bg-green-100 rounded-lg h-20 w-11/12 flex">
-            <div className=" bg-slate-50 rounded-lg h-20 w-5/6">
-              <div className=" text-slate-800 font-bold h-11 my-auto p-3 mx-1"> {station.name} </div>
-              <div className="flex p-1 h-9 ml-2">
-                <div className=" font-bold  border-slate-300 border-2 text-slate-900 text-center text-xs rounded-md w-5 h-5 mx-1"> 1 </div>
-                <div className=" font-bold  border-slate-300 border-2 text-slate-900 text-center text-xs rounded-md w-5 h-5 mx-1"> 2 </div>
-                <div className=" font-bold  border-slate-300 border-2 text-slate-900 text-center text-xs rounded-md w-5 h-5 mx-1"> 3 </div>
-                <div className=" font-bold  border-slate-300 border-2 text-slate-900 text-center text-xs rounded-md w-5 h-5 mx-1"> π </div>
-              </div>
+      {/* {stations?.map((station) => (
+        lines = station.stations_routes?.map(item => item.route_id) ,
+        <a href={'stations/' + station.station_id} className=" bg-orange-100 rounded-lg h-20 w-11/12 flex">
+          <div className=" bg-slate-50 rounded-lg h-20 w-5/6">
+            <div className=" text-slate-800 font-bold h-11 my-auto p-3 mx-1"> {station.station_name} </div>
+            <div className="flex p-1 h-9 ml-2">
+              {lines.includes(1) && <div className=" font-bold  border-slate-300 border-2 text-slate-900 text-center text-xs rounded-md w-5 h-5 mx-1"> 1 </div>}
+              {lines.includes(2) && <div className=" font-bold  border-slate-300 border-2 text-slate-900 text-center text-xs rounded-md w-5 h-5 mx-1"> 2 </div>}
+              {lines.includes(3) && <div className=" font-bold  border-slate-300 border-2 text-slate-900 text-center text-xs rounded-md w-5 h-5 mx-1"> 3 </div>}
+              {lines.includes(4) && <div className=" font-bold  border-slate-300 border-2 text-slate-900 text-center text-xs rounded-md w-5 h-5 mx-1"> π </div>}
             </div>
-            <div className="m-auto">
-              <Image src="/icons/checkcircle.svg"
-                    width={32}
-                    height={32}
-              />
-            </div>
-          </a>
-        ))}
+          </div>
+          <div className="m-auto">
+            <Image src="/icons/questionmarkcircle.svg"
+                  alt="icon"
+                  width={32}
+                  height={32}
+            />
+          </div>
+        </a>
+      ))} */}{JSON.stringify(stations)}
     </>
   )
    
@@ -36,3 +44,7 @@ const StationList = async () => {
 export default StationList
 
 // <div>{JSON.stringify(data)}</div>                             {station.name}
+
+
+
+// {JSON.stringify( station.stations_routes[1].'route_id' )}
