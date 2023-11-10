2fe2ca7847295198e63a3774a19683652d4873df
Harry Kaonis <10783823+kaonis@users.noreply.github.com>
2023-11-11 01:33:29 +0200

    Add loading

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,20 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { Header } from "@/components/Header";
-import { Footer } from "@/components/Footer";
+import { Suspense } from 'react';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ["latin"] });
+import './globals.css';
+import { Header } from '@/components/Header';
+import { Footer } from '@/components/Footer';
+
+import { LOADING_SPINNER_COLOR, LOADING_SPINNER_SECONDARY_COLOR } from '@/constants';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={"bg-slate-700"}>
+      <body className={'bg-slate-700'}>
         <Header />
-        {children}
+        <Suspense fallback={<p>Φόρτωση δεδομένων...</p>}>{children}</Suspense>
         <Footer />
       </body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,18 @@
+import { Suspense } from 'react';
 // import Message from '@/components/Message';
 import Map from '@/components/Map';
 import StationList from '@/components/StationList';
+
+import { LOADING_SPINNER_COLOR, LOADING_SPINNER_SECONDARY_COLOR } from '@/constants';
 
 export default function Home() {
   return (
     <main className="flex flex-grow flex-col items-center justify-center space-y-10">
       {/* <Message /> */}
       <Map />
-      <StationList />
+      <Suspense fallback={<p>Φόρτωση δεδομένων...</p>}>
+        <StationList />
+      </Suspense>
     </main>
   );
 }
