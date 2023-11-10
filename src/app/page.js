a75f031596c5c2ad8f057c126597207a9acc39b4
Harry Kaonis <10783823+kaonis@users.noreply.github.com>
2023-11-11 01:07:36 +0200

    Readd station list to home

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,17 @@
 import ClientComponentExample from '@/components/ClientComponentExample';
 import ServerComponentExample from '@/components/ServerComponentExample';
 import Link from 'next/link';
+import StaredStations from '@/components/StaredStations';
 import Message from '@/components/Message';
+import Map from '@/components/Map';
+import StationList from '@/components/StationList';
 
 export default function Home() {
   return (
-    <main className="flex flex-grow flex-col items-center justify-center space-y-6">
-      <Message />
-      <Link href="/stations"> stations </Link>
+    <main className="flex flex-grow flex-col items-center justify-center space-y-10">
+      {/* <Message /> */}
+      <Map />
+      <StationList />
     </main>
   );
 }
