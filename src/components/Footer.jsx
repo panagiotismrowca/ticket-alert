e4845da8f9a0c1c6aef8a33e0949097a9a184839
Harry Kaonis <10783823+kaonis@users.noreply.github.com>
2023-11-11 00:14:48 +0200

    Refactor map, footer & body styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,16 +3,12 @@ import Link from 'next/link';
 
 export const Footer = () => {
   return (
-    <footer className="bg-gray-900 p-4 text-white text-center fixed bottom-0 w-full mt-10 rounded-t-2xlzzz">
+    <footer className="flex bg-gray-900 p-4 text-white text-center bottom-0 sticky w-full mt-auto">
       <div className="container mx-auto">
-        <Link
-          href="https://github.com/panagiotismrowca"
-          className="text-blue-500 hover:underline"
-        >
+        <Link href="https://github.com/panagiotismrowca" className="text-blue-500 hover:underline">
           GitHub
-        </ Link>
+        </Link>
       </div>
     </footer>
   );
 };
-
