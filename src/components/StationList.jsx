05f4f6f11c6c58ed503b7d08b86a313cb1661777
Harry Kaonis <10783823+kaonis@users.noreply.github.com>
2023-11-10 23:11:18 +0200

    Improve station list

--- src/components/StationList.jsx
+++ src/components/StationList.jsx
@@ -1,78 +1,106 @@
-"use client"
+'use client';
 
-import { useState , useEffect } from "react"
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
-import Link from "next/link"
-import Image from "next/image"
+import { useState, useEffect, useRef } from 'react';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import Link from 'next/link';
+import Image from 'next/image';
+import { Oval } from 'react-loader-spinner';
+
+const LOADING_SPINNER_COLOR = '#0093D0';
+const LOADING_SPINNER_SECONDARY_COLOR = '#ffffff';
 
 const StationList = () => {
-    const supabase = createClientComponentClient();
-    const [stations, setStations] = useState();
-    const storedStations = JSON.parse(localStorage.getItem('stations'));
+  const supabase = createClientComponentClient();
 
-    useEffect(() => {
-        if (storedStations) {
-            setStations(storedStations);
-        } else {
-            getStations();
-        }
-    }, []);
+  const [stations, setStations] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const storedStations = useRef();
 
-    const getStations = async () => {
-        const { data, error } = await supabase.from('stations').select('*').order('station_name', 'asc');
+  useEffect(() => {
+    storedStations.current = JSON.parse(localStorage.getItem('stations'));
 
-        if (data) {
-            setStations(data);
-            localStorage.setItem('stations', JSON.stringify(data));
-        } else {
-            console.log(error);
-        }
-    };
-  
-    const searchStations = (keyword) => {
-        const initialStations = storedStations || stations;
-      
-        const normalizedKeyword = keyword.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-        
-        const filteredStations = initialStations.filter(station =>
-            station.station_name.toUpperCase().includes(normalizedKeyword.toUpperCase())
-        );
-      
-        setStations(filteredStations);
-    };
-  
+    if (storedStations.current) {
+      setStations(storedStations.current);
+      setIsLoading(false);
+    } else {
+      console.log('fetching stations');
+      getStations();
+    }
+  }, []);
 
+  const getStations = async () => {
+    const { data, error } = await supabase.from('stations').select('*').order('station_name', 'asc');
+
+    if (data) {
+      setStations(data);
+      localStorage.setItem('stations', JSON.stringify(data));
+      setIsLoading(false);
+    } else {
+      console.log(error);
+    }
+  };
+
+  const searchStations = (keyword) => {
+    const initialStations = storedStations.current ?? stations;
+
+    const normalizedKeyword = keyword.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+
+    const filteredStations = initialStations.filter((station) =>
+      station.station_name.toUpperCase().includes(normalizedKeyword.toUpperCase()),
+    );
+
+    setStations(filteredStations);
+  };
 
   return (
-      <>
-          <input 
-              className="transition-all duration-300 rounded-lg p-2 border outline-none  bg-slate-50 
+    <>
+      <input
+        className="transition-all duration-300 rounded-lg p-2 border outline-none  bg-slate-50
                     w-10/12       bg-opacity-10       placeholder:text-slate-50        text-slate-50
               focus:w-11/12 focus:bg-opacity-75 focus:placeholder:text-slate-950 focus:text-slate-950"
-              placeholder="Αναζήτηση" onChange={(e) => searchStations(e.target.value)} type="text" 
-          />
+        placeholder="Αναζήτηση"
+        onChange={(e) => searchStations(e.target.value)}
+        type="text"
+        aria-label="Αναζήτηση"
+      />
 
-          {stations?.map((station) => (
-              <Link href={'stations/' + station.station_id} className=" bg-slate-800 rounded-lg h-20 w-11/12 lg:w-8/12 flex">
-                  <div className=" bg-slate-50 rounded-lg h-20 w-5/6">
-                      <div className=" text-slate-800 font-bold h-11 my-auto p-3 mx-1"> {station.station_name} </div>
-                      <div className="flex p-1 h-9 ml-2">
-                          {station.routes.includes(1) && <div className="font-bold  border-slate-300 border-2 text-slate-900 text-center text-xs rounded-md w-5 h-5 mx-1"> 1 </div>}
-                          {station.routes.includes(2) && <div className="font-bold  border-slate-300 border-2 text-slate-900 text-center text-xs rounded-md w-5 h-5 mx-1"> 2 </div>}
-                          {station.routes.includes(3) && <div className="font-bold  border-slate-300 border-2 text-slate-900 text-center text-xs rounded-md w-5 h-5 mx-1"> 3 </div>}
-                      </div>
-                  </div>
-                  <div className="m-auto">
-                      <Image src="/icons/arrow-small-right.svg"
-                             alt="icon"
-                             width={32}
-                             height={32}
-                      />
-                  </div>
-              </Link>
-          ) ) }
-      </>
-    )
-}
+      {isLoading ? (
+        <div className="flex justify-center items-center h-96">
+          <Oval color={LOADING_SPINNER_COLOR} secondaryColor={LOADING_SPINNER_SECONDARY_COLOR} />
+        </div>
+      ) : (
+        stations?.length === 0 && <div className="flex justify-center items-center h-96 text-gray-50">Δεν βρέθηκαν στάσεις</div>
+      )}
 
-export default StationList
+      {stations &&
+        stations?.length > 0 &&
+        stations?.map((station, index) => (
+          <Link
+            key={`station_${index}`}
+            href={'stations/' + station.station_id}
+            className=" bg-slate-800 rounded-lg h-20 w-11/12 lg:w-8/12 flex"
+          >
+            <div className=" bg-slate-50 rounded-lg h-20 w-5/6">
+              <div className=" text-slate-800 font-bold h-11 my-auto p-3 mx-1"> {station.station_name} </div>
+              <div className="flex p-1 h-9 ml-2">
+                {station.routes.includes(1) && (
+                  <div className="font-bold  border-slate-300 border-2 text-slate-900 text-center text-xs rounded-md w-5 h-5 mx-1"> 1 </div>
+                )}
+                {station.routes.includes(2) && (
+                  <div className="font-bold  border-slate-300 border-2 text-slate-900 text-center text-xs rounded-md w-5 h-5 mx-1"> 2 </div>
+                )}
+                {station.routes.includes(3) && (
+                  <div className="font-bold  border-slate-300 border-2 text-slate-900 text-center text-xs rounded-md w-5 h-5 mx-1"> 3 </div>
+                )}
+              </div>
+            </div>
+            <div className="m-auto">
+              <Image src="/icons/arrow-small-right.svg" alt="icon" width={32} height={32} />
+            </div>
+          </Link>
+        ))}
+    </>
+  );
+};
+
+export default StationList;
