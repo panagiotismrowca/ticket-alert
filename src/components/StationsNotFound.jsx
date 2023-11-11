d64d6a146b3b113f4718f2eb31247d08f833da88
Harry Kaonis <10783823+kaonis@users.noreply.github.com>
2023-11-11 21:18:14 +0200

    Update stations not found padding

--- src/components/StationsNotFound.jsx
+++ src/components/StationsNotFound.jsx
@@ -1,6 +1,6 @@
 export default function StationsNotFound() {
   return (
-    <div className="flex flex-col items-center justify-center ">
+    <div className="flex flex-col pt-6 pb-2 items-center justify-center ">
       <div className="text-gray-300 font-bold text-xl">Δεν βρέθηκαν στάσεις</div>
       <div className="text-gray-300 font-bold text-md">Δοκιμάστε ξανά</div>
     </div>
