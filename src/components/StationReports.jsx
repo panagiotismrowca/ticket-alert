1b55e92d1b4769bd6ad2a50682abc4377de3054b
panagiotismrowca <89362761+panagiotismrowca@users.noreply.github.com>
2023-11-12 19:24:29 +0200

    2

--- src/components/StationReports.jsx
+++ src/components/StationReports.jsx
@@ -1,36 +1,48 @@
-import { cookies } from "next/headers"
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
+'use client';
+import Image from "next/image";
 
-const StationReports = async () => {
-  const supabase = createServerComponentClient({ cookies })
+const StationReports = ({ station_reports : reports }) => {
+  console.log(reports)
+  console.log(reports.total_reports_with_controllers)
+  console.log(reports.total_reports_without_controllers)
 
-  const { data, error } = await supabase.from("stations").select("*")
+  if(!(reports.total_reports_with_controllers>0 && reports.total_reports_without_controllers>0)){
+    // εάν δεν υπάρχουν αναφορές καλείται συνάρτηση που υπολογίζει πιθανότητες
+    console.log('statistica')
+  } else {
+    reports_balance = reports.total_reports_without_controllers - reports.total_reports_with_controllers;
+    if(reports_balance < 0){
+      // εάν η ισορροπία του σταθμού ειναι αρνητικη υπάρχουν περισσοτερες αναφορές για ελεγκτές
+      if(reports_balance < 1){
+        ;
+      }
+    }
 
-  return(
-    <>
-        <div className="m-0 bg-white backdrop:blur-md flex justify-center space-x-3">
-          <div className="bg-red-200 p-8">
-            EXEI
-          </div>
-          <div className="bg-red-200 p-8">
-            EXEI
-          </div>
-          <div className="bg-red-200 p-8">
-            EXEI
-          </div>
+  }
 
+  
+  return (
+    <div className="w-full">
+      <div className="p-2 flex bg-slate-50 justify-center">
+        <div className="text-slate-800 font-black h-22 p-6 text-2xl w-7/12">
+          {reports.station_name} 
         </div>
-        {data.map((data) => (
-          <div key={data._id}>
-            <h1>{data.name}</h1>
-            <h1>{data.open_at}</h1>
+        <div className="text-slate-800 font-black h-22 p-6 text-center text-2xl w-5/12">
+        <div className="flex bg-slate-800 text-slate-50 rounded-t-md">
+            <div className="w-1/2">2</div>
+            <div className="w-1/2">3</div>
           </div>
-        ))}
-    </>
-  )
-   
-}
+          <div className="rounded-b-md border-slate-800 border-2 text-red-800">
+            EXEI
+          </div>
+        </div>
+      </div>
+      <div className="flex p-2 bg-slate-50 font-medium">
+        <button className="border-4 border-green-700 text-green-700 w-1/3 p-2 rounded-3xl mx-auto"> DEN EXEI </button>
+        <button className="border-4 border-red-700 text-red-700 w-1/3 rounded-3xl p-4 mx-auto"> EXEI </button>
+      </div>
+    </div>
+  );
+};
 
-export default StationReports
-
-// <div>{JSON.stringify(data)}</div>
+export default StationReports;
