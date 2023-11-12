f77ad1bf12010cc8bb0ddfd1ee886f39c9aebb12
panagiotismrowca <89362761+panagiotismrowca@users.noreply.github.com>
2023-11-12 19:23:44 +0200

    1

--- src/app/stations/[id]/page.jsx
+++ src/app/stations/[id]/page.jsx
@@ -1,10 +1,13 @@
 import StationReports from '@/components/StationReports'
+import { getStationReportsServer } from '@/lib/stationService';
 
-export default function Page({ params }) {
+export default async function Station({ params }) {
+  const station_reports = await getStationReportsServer(params.id);
+  console.log(station_reports)
 
-  
-  return(
-    <StationReports />
-  ) 
-
+  return (
+    <main className="flex flex-grow flex-col items-center justify-center space-y-10">
+      <StationReports station_reports={station_reports} />
+    </main>
+  );
 }
